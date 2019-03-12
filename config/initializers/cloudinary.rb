651a31b10efd72b8a8ca9120290468dd2e6427a2
Serge Bocancea <bocanceaserge335566@gmail.com>
2019-03-12 11:05:25 +0100

    Cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+Cloudinary.config do |config|
+  config.cloud_name = '[les-associes]'
+  config.api_key = ENV['731264298985222']
+  config.api_secret = ENV['7klXclMY3SzWychpiBm9F8iiiZ8']
+  config.secure = true
+  config.cdn_subdomain = true
+end
+
