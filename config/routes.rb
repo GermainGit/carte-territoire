4c52f778a2ec88bfcf2062cf52d6ff3249df9be4
Germain <germain.prevot@gmail.com>
2019-03-13 17:10:44 +0100

    page profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'users/:id' => 'users#show'
   resources :photos
   get "/staticpages/:staticpage" => "staticpages#show", as: :static
   root to: 'photos#index'
