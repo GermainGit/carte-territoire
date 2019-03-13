b12d4cdbfe3b57cba8168480ae1c0a45116a05c4
Hugo LEFRANT <hugo.lefrant@gmail.com>
2019-03-13 14:00:14 +0100

    require img et inscription

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,10 @@
 class Photo < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :date, presence: true
+  validates :long, presence: true
+  validates :lat, presence: true
+  validates :file, presence: true
   belongs_to :user
   mount_uploader :file, FileUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :prenom, presence: true
+  validates :nom, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
   has_many :photos
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
