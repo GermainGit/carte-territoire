aa132f372826d563727fb711b045f0ec9fea8834
debian <hugopichard>
2019-03-13 12:34:01 +0100

    mise en form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :photos
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :photos
   get "/staticpages/:staticpage" => "staticpages#show", as: :static
   root to: 'photos#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
