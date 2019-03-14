c48ff2f34afbaccc9df7298fb386b74d2bf8d685
Serge Bocancea <bocanceaserge335566@gmail.com>
2019-03-14 09:31:00 +0100

    fix ujs errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+
 //= require_tree .
 //= require leaflet
+//= require turbolinks
+//= require rails-ujs
 
